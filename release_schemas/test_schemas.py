f290c13abaf069d5786d5d1f929af9076dbf645e
ecowan <ethan24@gmail.com>
2020-09-25 13:50:35 -0400

    Report schema and test function

--- release_schemas/test_schemas.py
+++ release_schemas/test_schemas.py
@@ -3,4 +3,24 @@ Test all schemas and their related example files for validity.
 
 ref: https://github.com/Julian/jsonschema
 """
-# Placeholder 
+
+import json
+
+from jsonschema import validate
+
+
+def validate_report(instance):
+
+    with open('version-0.1.0-draft/release-schema.json', 'r') as infile:
+        schema = json.load(infile)
+
+    return validate(instance=instance, schema=schema)
+
+
+if __name__ == '__main__':
+
+    with open('version-0.1.0-draft/release-example-files/example_01.json', 'r') as infile:
+        report = json.load(infile)
+
+    validate_report(report)
+    print("Validation succeeded")
